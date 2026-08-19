c446eceafce96521f9270e899b7fba3a85c43777
supriyadhana <dsupriyadec2004@gmail.com>
2026-08-19 23:35:19 +0530

    Update Project 3 SQL queries

--- project3_sql_analysis.sql
+++ project3_sql_analysis.sql
@@ -1,2 +1,47 @@
 SELECT *
 FROM orders;
+
+SELECT OrderID, Product, Quantity, TotalPrice
+FROM orders;
+
+SELECT *
+FROM orders
+WHERE OrderStatus = 'Delivered';
+
+SELECT OrderID, Product, TotalPrice
+FROM orders
+WHERE TotalPrice > 2000;
+
+SELECT *
+FROM orders
+ORDER BY TotalPrice DESC;
+
+SELECT Product, Quantity, TotalPrice
+FROM orders
+ORDER BY Quantity DESC;
+
+
+SELECT Product, COUNT(*) AS OrderCount
+FROM orders
+GROUP BY Product;
+
+SELECT PaymentMethod, COUNT(*) AS OrderCount
+FROM orders
+GROUP BY PaymentMethod
+ORDER BY OrderCount DESC;
+
+SELECT
+    COUNT(*) AS TotalOrders,
+    AVG(Quantity) AS AverageQuantity,
+    SUM(TotalPrice) AS TotalSales,
+    AVG(TotalPrice) AS AverageOrderValue
+FROM orders;
+
+SELECT
+    Product,
+    COUNT(*) AS OrderCount,
+    SUM(TotalPrice) AS TotalSales,
+    AVG(TotalPrice) AS AverageSales
+FROM orders
+GROUP BY Product
+ORDER BY TotalSales DESC;
